10ed6589e2435a68778edd1938e42017d49f12c4
Jeremy Magland <jeremy.magland@gmail.com>
2014-07-21 04:40:33 -0400

    wisdmconfig

--- wisdmconfig_template.js
+++ wisdmconfig_template.js
@@ -2,32 +2,17 @@
 
 var wisdmconfig={};
 
-wisdmconfig.processing_node={
-	node_id:'node1',
-	node_path:'/home/magland/processing_nodes/node1',
-	server_host:'localhost',
-	server_port:8081
-};
-
 wisdmconfig.wisdm_server={
-	listen_port:8000,
-	www_path:'/home/magland/public_html/magland.org/public',
-	server_source_path:'/home/magland/dev/nodejs',
-	users_path:'/home/magland/.wisdm/users.json',
-	auth_path:'/tmp/wisdmauth',
-	processingwebserver_url:'http://localhost:8001',
-	github_client_id:'',
-	github_client_secret:'',
-	google_client_id:'',
-	google_client_secret:''
-};
-
-wisdmconfig.processing_server={
-	listen_port:8081
-};
-
-wisdmconfig.wisdmprocessing={
-	source_path:'/home/magland/dev/nodejs/wisdmprocessing'
+        listen_port:8080,
+        www_path:'/home/magland/wisdm/www',
+        //server_source_path:'/home/magland/dev/nodejs',
+        processingwebserver_url:'http://wisdmhub.org:8081',
+        users_path:'/home/magland/.wisdm/users.json',
+        auth_path:'/tmp/wisdmauth',
+        github_client_id:'',
+        github_client_secret:'',
+        google_client_id:'',
+        google_client_secret:''
 };
 
 exports.wisdmconfig=wisdmconfig;
